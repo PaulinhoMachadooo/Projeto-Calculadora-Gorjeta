60d32d06df0f6326479147baf94493ff027fb237
PaulinhoMachadooo <paulinhomachadoo20@gmail.com>
2021-03-23 12:29:11 -0300

    Inicio

--- App.js
+++ App.js
@@ -1,21 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, View, Text, Button } from 'react-native';
+import styled from 'styled-components/native';
 
-export default function App() {
+
+const Page = styled.SafeAreaView`
+  flex:1;
+  justify-content:center;
+  align-items:center;
+`;
+
+const HeaderText = styled.Text`
+  font-size:25px;
+`;
+
+const Input = styled.TextInput`
+  width:90%;
+  height:40px;
+  font-size:18px;
+  background-color:#EEE;
+  margin-top:20px;
+  margin-bottom:20px;
+  border-radius:10px;
+  padding:10px;
+`;
+
+const CalcButton = styled.Button`
+  margin-top:10px;
+`;
+
+const ResultArea = styled.View`
+  width:100%;
+  margin-top:30px;
+  background-color:#EEE;
+  padding:20px;
+  justify-content:center;
+  align-items:center;
+`;
+
+const ResultItemTitle = styled.Text`
+  font-size:18px;
+  font-weight:bold;
+`;
+
+const ResultItem = styled.Text`
+  font-size:15px;
+  margin-bottom:30px;
+`;
+
+export default () => {
+
+  const [bill, setBill] = useState('');
+  const [tip, setTip] = useState(0);
+
+  const calc = () => {
+    let nBill = parseFloat(bill);
+
+    if(nBill) {
+      setTip( 10/100 * nBill );
+    } else {
+      alert("Digite o valor da conta");
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Page>
+      <HeaderText>Calculadora de Gorjeta</HeaderText>
+      <Input
+        placeholder="Quanto deu a conta?"
+        placeholderTextColor="#000"
+        keyboardType="numeric" /* Tipo de teclado numerico*/
+        value={bill}
+        onChangeText={n=>setBill(n)} /* Podendo alterar os valores*/
+      />
+      <CalcButton title="Calcular" onPress={calc}/>
+      {tip > 0 &&
+      <ResultArea>
+        <ResultItemTitle>Valor da Conta</ResultItemTitle>
+        <ResultItem>R$ {parseFloat(bill).toFixed(2)}</ResultItem>
+
+        <ResultItemTitle>Valor da Gorjeta</ResultItemTitle>
+        <ResultItem>R$ {tip.toFixed(2)} (10%) </ResultItem>
+
+        <ResultItemTitle>Valor Total</ResultItemTitle>
+        <ResultItem>R$ {(parseFloat (bill) + tip).toFixed(2)}</ResultItem>
+      </ResultArea>
+}
+    </Page>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
